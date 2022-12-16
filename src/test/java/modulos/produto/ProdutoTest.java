49629307f66f6d7194dc083d8463afaec5dad3d7
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-12-15 23:09:31 -0300

    Preparação do projeto

---
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -0,0 +1,14 @@
+package modulos.produto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testes de API Rest do modulo de Produto")
+public class ProdutoTest {
+    @Test
+    @DisplayName("Validar os limites proibidos do valor do Produto")
+    public void testValidarLimitesProibidosValorProduto(){
+
+    }
+}
