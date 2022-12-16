1061bdb20615873773d01a15f8cef9bbbe7afc61
Goku (João Vitor dos Santos) <qakarotto@gmail.com>
2022-12-16 18:31:15 -0300

    Testes de API do módulo de produto com DataFactories

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -1,14 +1,60 @@
 package modulos.produto;
 
-import org.junit.jupiter.api.Assertions;
+import dataFactory.ProdutoDataFactory;
+import dataFactory.UsuarioDataFactory;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 @DisplayName("Testes de API Rest do modulo de Produto")
 public class ProdutoTest {
-    @Test
-    @DisplayName("Validar os limites proibidos do valor do Produto")
-    public void testValidarLimitesProibidosValorProduto(){
+    private String token;
+    @BeforeEach
+    public void beforeEach() {
+        baseURI = "http://165.227.93.41";
+        basePath = "/lojinha";
 
+        this.token = given()
+                .contentType(ContentType.JSON)
+                .body(UsuarioDataFactory.criarUsuarioAdministrador())
+            .when()
+                .post("/v2/login")
+            .then()
+                .extract()
+                    .path("data.token");
+    }
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 0.00 não é permitido")
+    public void testValidarLimiteZeradoProibidoValorProduto(){
+        given()
+                .header("token", this.token)
+                .contentType(ContentType.JSON)
+                .body(ProdutoDataFactory.criarProdutoComumComOValorIgualA(0.00))
+        .when()
+                .post("/v2/produtos")
+        .then()
+            .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+    }
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 7000.01 não é permitido")
+    public void testValidarLimiteMaiorQue7000ProibidoValorProduto(){
+        given()
+            .header("token", this.token)
+            .contentType(ContentType.JSON)
+            .body(ProdutoDataFactory.criarProdutoComumComOValorIgualA(7000.01))
+        .when()
+                .post("/v2/produtos")
+        .then()
+            .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
     }
 }
---
+++ src/test/java/pojo/UsuarioPojo.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class UsuarioPojo {
+    public String getUsuarioLogin() {
+        return usuarioLogin;
+    }
+
+    public void setUsuarioLogin(String usuarioLogin) {
+        this.usuarioLogin = usuarioLogin;
+    }
+
+    public String getUsuarioSenha() {
+        return usuarioSenha;
+    }
+
+    public void setUsuarioSenha(String usuarioSenha) {
+        this.usuarioSenha = usuarioSenha;
+    }
+
+    private String usuarioLogin;
+    private String usuarioSenha;
+}
